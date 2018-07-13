a817fec1c13b8ac5617b1ad464198f724087fcbc
unknown <qdoan97@gmail.com>
2018-07-13 00:44:24 -0500

    Updated the play function to add tens place minutes

--- AlarmClock/Time_Telling.py
+++ AlarmClock/Time_Telling.py
@@ -5,6 +5,8 @@ import datetime
 #this will be the current time 
 now = datetime.datetime.now()
 
+instance = vlc.Instance()
+
 #this will get the current hour  
 def hour():
     hour = now.hour 
@@ -21,11 +23,28 @@ def minute():
     return minute
 
 #this function will be able to play the sound of the time 
-def play(hour, minute):
-    print(hour)
+def play(command):
+    
+    if command == "hour":
+        file_Path = str(hour()) + ".wav"
+    elif command == "tens_minute":
+        tens_minute = str(minute())
+        file_Path = tens_minute[0]+ "0" + ".wav"
+
+    #Create a MediaPlayer with the default instance
+    player = instance.media_player_new()
+
+    #Load the media file
+    media = instance.media_new(file_Path)
+
+    #Add the media to the player
+    player.set_media(media)
+
+    player.play()
+    time.sleep(1)
 
 def main():
     
-    play(hour(), minute())
+    play("tens_minute")
     
 main()
