2ecb4ca6443efc24c2840ce5bc5e0e6b26e5d239
unknown <qdoan97@gmail.com>
2018-07-13 00:56:34 -0500

    simple alarm clock

--- AlarmClock/Time_Telling.py
+++ AlarmClock/Time_Telling.py
@@ -7,14 +7,17 @@ now = datetime.datetime.now()
 
 instance = vlc.Instance()
 
+am_pm = "am"
 #this will get the current hour  
 def hour():
     hour = now.hour 
     if hour > 12:
         hour = hour - 12
+        am_pm = "pm"
     #this is for the case of when it is 12 am 
     elif hour == 0:
         hour = 12
+        am_pm = "am"
     return hour
 
 #this will get the current minute
@@ -38,6 +41,10 @@ def play(command):
         file_Path = "its.wav"
     elif command == "o":
         file_Path = "o.wav"
+    elif command == "am":
+        file_Path = "am.wav"
+    elif command == "pm":
+        file_Path = "pm.wav"
 
     #Create a MediaPlayer with the default instance
     player = instance.media_player_new()
@@ -53,6 +60,10 @@ def play(command):
 
 def main():
     
+    play("its")
+    play("hour")
+    play("tens_minute")
     play("ones_minute")
-    
+    play(am_pm)
+
 main()
