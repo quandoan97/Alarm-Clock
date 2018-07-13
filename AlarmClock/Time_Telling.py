030085449e44462787b6ccedbd751cad32c9fc7e
unknown <qdoan97@gmail.com>
2018-07-13 00:18:46 -0500

    Added the case for when it is 12 am

--- AlarmClock/Time_Telling.py
+++ AlarmClock/Time_Telling.py
@@ -10,6 +10,9 @@ def hour():
     hour = now.hour 
     if hour > 12:
         hour = hour - 12
+    #this is for the case of when it is 12 am 
+    elif hour == 0:
+        hour = 12
     return hour
 
 #this will get the current minute
@@ -17,8 +20,12 @@ def minute():
     minute = now.minute
     return minute
 
+#this function will be able to play the sound of the time 
+def play(hour, minute):
+    print(hour)
+
 def main():
     
-        
+    play(hour(), minute())
     
 main()
